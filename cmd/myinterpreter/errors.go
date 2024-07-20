dfe147a9e2db64d9a6f0bd8214c1091d6bc0637c
Francisco Flores <francisco.giavay@gmail.com>
2024-07-20 11:51:30 -0300

    handling unexpected chars func impl 5

--- cmd/myinterpreter/errors.go
+++ cmd/myinterpreter/errors.go
@@ -6,6 +6,6 @@ import (
 )
 
 func ReportError(line int, message string) {
-	output := fmt.Sprintf("[Line %d] Error: %s", line, message)
+	output := fmt.Sprintf("[line %d] Error: %s", line, message)
 	fmt.Fprintln(os.Stderr, output)
 }
