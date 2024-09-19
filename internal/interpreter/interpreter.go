8b0a60272ceb881ad34250c99accb9f1b7192b62
Francisco Flores <francisco.giavay@gmail.com>
2024-09-19 18:34:30 -0300

    equality operators impl 1

--- internal/interpreter/interpreter.go
+++ internal/interpreter/interpreter.go
@@ -14,15 +14,21 @@ type Interpreter struct {
 	expr *parser.Node
 }
 
+type result struct {
+	Value     string
+	valueType scanner.TokenType
+}
+
 func NewInterpreter(expr *parser.Node) *Interpreter {
 	return &Interpreter{expr: expr}
 }
 
 func (inter *Interpreter) Interpret() (string, error) {
-	return evaluate(inter.expr)
+	result, err := evaluate(inter.expr)
+	return result.Value, err
 }
 
-func evaluate(expr *parser.Node) (string, error) {
+func evaluate(expr *parser.Node) (result, error) {
 
 	switch expr.ExprType {
 	case parser.BINARY:
@@ -36,125 +42,132 @@ func evaluate(expr *parser.Node) (string, error) {
 	}
 }
 
-func evaluateBinary(expr *parser.Node) (string, error) {
+func evaluateBinary(expr *parser.Node) (result, error) {
 	left, err := evaluate(expr.Left)
 	if err != nil {
-		return "", err
+		return result{}, err
 	}
+
 	right, err := evaluate(expr.Right)
 	if err != nil {
-		return "", err
+		return result{}, err
 	}
 
-	if !isNumber(left) || !isNumber(right) {
-		return left + right, nil
+	// evaluate equality operators
+	if expr.Value.TokenType == scanner.EQUAL_EQUAL {
+		if left.valueType != right.valueType {
+			return result{"false", scanner.FALSE}, nil
+		} else if left.Value != right.Value {
+			return result{"false", scanner.FALSE}, nil
+		}
+		return result{"true", scanner.TRUE}, nil
+	} else if expr.Value.TokenType == scanner.BANG_EQUAL {
+		if left.valueType != right.valueType {
+			return result{"true", scanner.TRUE}, nil
+		} else if left.Value != right.Value {
+			return result{"true", scanner.TRUE}, nil
+		}
+		return result{"false", scanner.FALSE}, nil
 	}
 
-	nLeft, err := strconv.ParseFloat(left, 64)
+	if left.valueType != scanner.NUMBER || right.valueType != scanner.NUMBER {
+		return result{left.Value + right.Value, scanner.STRING}, nil
+	}
+
+	nLeft, err := strconv.ParseFloat(left.Value, 64)
 	if err != nil {
 		panic(err)
 	}
-	nRight, err := strconv.ParseFloat(right, 64)
+	nRight, err := strconv.ParseFloat(right.Value, 64)
 	if err != nil {
 		panic(err)
 	}
 
-	var result float64
-	var strResult string
-	aritmeticOper := false
+	var res float64
 
-	switch expr.Value.Lexeme {
-	case "-":
-		result = nLeft - nRight
-		aritmeticOper = true
-	case "*":
-		result = nLeft * nRight
-		aritmeticOper = true
-	case "/":
-		if nRight == 0 {
-			return "", errors.New("division by cero")
-		}
-		result = nLeft / nRight
-		aritmeticOper = true
-	case "+":
-		result = nLeft + nRight
-		aritmeticOper = true
-	case "<":
-		if nLeft < nRight {
-			strResult = "true"
-		} else {
-			strResult = "false"
-		}
-	case "<=":
-		if nLeft <= nRight {
-			strResult = "true"
-		} else {
-			strResult = "false"
-		}
-	case ">":
-		if nLeft > nRight {
-			strResult = "true"
-		} else {
-			strResult = "false"
-		}
-	case ">=":
-		if nLeft >= nRight {
-			strResult = "true"
-		} else {
-			strResult = "false"
-		}
-	}
-	if aritmeticOper {
-		truncRes := int64(result)
-		if result > float64(truncRes) {
-			return fmt.Sprintf("%g", result), nil
-		}
-		return fmt.Sprintf("%.0f", result), nil
-	}
-	return strResult, nil
-}
-
-func evaluateLiteral(expr *parser.Node) (string, error) {
 	switch expr.Value.TokenType {
-	case scanner.TRUE:
-		return "true", nil
-	case scanner.FALSE:
-		return "false", nil
-	case scanner.NIL:
-		return "nil", nil
-	case scanner.STRING:
-		return expr.Value.Literal, nil
-	case scanner.NUMBER:
-		return evaluateNumber(expr.Value.Literal), nil
+	case scanner.MINUS:
+		res = nLeft - nRight
+		return result{formatResultNum(res), scanner.NUMBER}, nil
+	case scanner.STAR:
+		res = nLeft * nRight
+		return result{formatResultNum(res), scanner.NUMBER}, nil
+	case scanner.SLASH:
+		if nRight == 0 {
+			return result{}, errors.New("division by cero")
+		}
+		res = nLeft / nRight
+		return result{formatResultNum(res), scanner.NUMBER}, nil
+	case scanner.PLUS:
+		res = nLeft + nRight
+		return result{formatResultNum(res), scanner.NUMBER}, nil
+	case scanner.LESS:
+		if nLeft < nRight {
+			return result{"true", scanner.TRUE}, nil
+		} else {
+			return result{"false", scanner.FALSE}, nil
+		}
+	case scanner.LESS_EQUAL:
+		if nLeft <= nRight {
+			return result{"true", scanner.TRUE}, nil
+		} else {
+			return result{"false", scanner.FALSE}, nil
+		}
+	case scanner.GREATER:
+		if nLeft > nRight {
+			return result{"true", scanner.TRUE}, nil
+		} else {
+			return result{"false", scanner.FALSE}, nil
+		}
+	case scanner.GREATER_EQUAL:
+		if nLeft >= nRight {
+			return result{"true", scanner.TRUE}, nil
+		} else {
+			return result{"false", scanner.FALSE}, nil
+		}
 	}
-	return "", nil
+	return result{}, errors.New("error in binary evaluation")
 }
 
-func evaluateUnary(expr *parser.Node) (string, error) {
-	result, err := evaluate(expr.Right)
+func evaluateUnary(expr *parser.Node) (result, error) {
+	res, err := evaluate(expr.Right)
 
 	if err != nil {
-		return "", err
+		return result{}, err
 	}
 
 	if expr.Value.Lexeme == "-" {
-		if isNumber(result) {
-			if result[0] == '-' {
-				return result[1:], nil
+		if res.valueType == scanner.MINUS {
+			if res.Value[0] == '-' {
+				return result{res.Value[1:], scanner.NUMBER}, nil
 			}
 		}
-
-		return "-" + result, nil
+		return result{"-" + res.Value, scanner.NUMBER}, nil
 	}
 
-	if isTruthy(result) {
-		return "false", nil
-	} else {
-		return "true", nil
+	if isTruthy(res.Value) {
+		return result{"false", scanner.FALSE}, nil
 	}
+	return result{"true", scanner.TRUE}, nil
 }
 
-func evaluateGrouping(expr *parser.Node) (string, error) {
+func evaluateLiteral(expr *parser.Node) (result, error) {
+	switch expr.Value.TokenType {
+	case scanner.TRUE:
+		return result{"true", scanner.TRUE}, nil
+	case scanner.FALSE:
+		return result{"false", scanner.FALSE}, nil
+	case scanner.NIL:
+		return result{"nil", scanner.NIL}, nil
+	case scanner.STRING:
+		return result{expr.Value.Literal, scanner.STRING}, nil
+	case scanner.NUMBER:
+		return result{evaluateNumber(expr.Value.Literal), scanner.NUMBER}, nil
+	}
+	return result{}, errors.New("should not happend")
+}
+
+func evaluateGrouping(expr *parser.Node) (result, error) {
 	return evaluate(expr.Left)
 }
 
@@ -165,6 +178,14 @@ func evaluateNumber(number string) string {
 		}
 	}
 	return number
+}
+
+func formatResultNum(number float64) string {
+	truncRes := int64(number)
+	if number > float64(truncRes) {
+		return fmt.Sprintf("%g", number)
+	}
+	return fmt.Sprintf("%.0f", number)
 }
 
 func isTruthy(value string) bool {
